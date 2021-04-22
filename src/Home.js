5b16db496a95dd6b5e6da7fa61c5bdedaefe923d
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-22 13:53:29 +0200

    home in progress

--- src/Home.js
+++ src/Home.js
@@ -11,8 +11,8 @@ const Home = () => {
                           <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                           <div className="slider-text-inner js-fullheight">
                               <div className="desc">
-                              <h1>Hi! <br />I'm Jackson</h1>
-                              <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
+                              <h1>Hi! <br />I'm Amine</h1>
+                              <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank" rel="noreferrer">colorlib.com</a></h2>
                               <p><a className="btn btn-primary btn-learn">Download CV <em className="icon-download4" /></a></p>
                               </div>
                           </div>
@@ -27,8 +27,8 @@ const Home = () => {
                           <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                           <div className="slider-text-inner">
                               <div className="desc">
-                              <h1>I am <br />a Designer</h1>
-                              <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
+                              <h1>I am <br />a Developper</h1>
+                              <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank" rel="noreferrer">colorlib.com</a></h2>
                               <p><a className="btn btn-primary btn-learn">View Portfolio <em className="icon-briefcase3" /></a></p>
                               </div>
                           </div>
